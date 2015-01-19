a1cd7a95ea80b0a1e34a4bbf5626e483044e64f5
dizhifeng <dizhifengminz@gmail.com>
2015-01-19 20:32:49 +0800

    remove server

--- server/src/main/java/sebms/repository/SpecialtyDao.java
+++
@@ -1,9 +0,0 @@
-package sebms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import sebms.entity.Specialty;
-
-public interface SpecialtyDao extends JpaRepository<Specialty, Long> {
-
-}
